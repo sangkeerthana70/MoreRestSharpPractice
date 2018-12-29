d8f7a3f62e6cb435bc3af759060f2fcc361e6e65
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-12-29 00:13:22 -0500

    Using Rest Sharp to get data from WeatherAPi.org

--- MoreRestSharpPractice/Program.cs
+++ MoreRestSharpPractice/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +13,27 @@ namespace MoreRestSharpPractice
     {
         static void Main(string[] args)
         {
+            var client = new RestClient("https://samples.openweathermap.org/data/2.5");
+
+            // create a new request
+            var request = new RestRequest("/weather?zip=02093,us&appid=b6907d289e10d714a6e88b30761fae22");
+
+            // execute the request
+            IRestResponse response = client.Execute(request);
+            var content = response.Content; // raw content as string
+            Console.WriteLine("Content : " + content);
+
+            // parses a JSON array from a string using JArray.Parse(String).
+            //JArray a = JArray.Parse(content);
+
+            //Console.WriteLine("String a: " + a);
+
+
+            //Deserialize will convert the raw string into Json format
+            var output = JsonConvert.DeserializeObject(content);
+            Console.WriteLine("Output: " + output);
+
+            
         }
     }
 }
