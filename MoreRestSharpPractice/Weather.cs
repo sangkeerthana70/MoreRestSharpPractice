8e5a805ff781123d0d45b8c953bf697b59be619e
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-01-01 20:14:23 -0500

    Create a Weather .cs to convert Json into an Object

---
+++ MoreRestSharpPractice/Weather.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreRestSharpPractice
+{
+    class Weather
+    {
+        public string name { get; set; }
+        public float temp { get; set; }
+        public Dictionary<string, float> main { get; set; }
+        public int visibility { get; set; }
+        public Dictionary<string,float> wind { get; set; }
+        public List<Dictionary<string,string>> weather { get; set; }
+    }
+}
